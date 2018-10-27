38d237712e2ec321b647af50f331301f457c9aac
Akhil Mohan <akhilerm@gmail.com>
2018-10-27 09:26:42 +0400

    Sorting function implemented

--- MinimumUniqueSum.java
+++ MinimumUniqueSum.java
@@ -1,3 +1,31 @@
 public class MinimumUniqueSum {
-	public
+	public static void main(String args[]) {
+        int array[] = {3,1,2,7,2};
+        System.out.println(getMinimumUniqueSum(5,array));
+	}
+
+	private static int getMinimumUniqueSum(int n, int arr[]) {
+        int sum = 0;
+        int i, j;
+
+        sort(arr,n);
+
+        
+
+        return sum;
+	}
+
+	static void sort(int arr[], int length) {
+	    int i, j;
+	    int temp;
+	    for (i = 0;i < length;i++) {
+	        for (j = i+1;j < length;j++) {
+	            if (arr[i] > arr[j]) {
+	                temp = arr[i];
+	                arr[i] = arr[j];
+	                arr[j] = temp;
+                }
+            }
+        }
+    }
 }
